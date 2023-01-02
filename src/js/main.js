7999329688ba59decd1a3c0410bd0f8c6486e7e1
Konstantin Orlov <konstorlovrus@gmail.com>
2023-01-02 12:47:08 +0300

    created timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,15 @@ import modals from "./modules/modals.js";
 import tabs from "./modules/tabs.js";
 import forms from "./modules/forms.js";
 import changeModalState from "./modules/changeModalState.js";
+import timer from "./modules/timer.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
   let modalState = {};
+  let deadline = '2023-12-18';
+
+
 
   changeModalState(modalState);
   modals();
@@ -15,4 +19,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 });
